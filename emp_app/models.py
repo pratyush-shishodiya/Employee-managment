e0ad81f9e4723d09c32dc2da3bb1f67e90537479
Pratyush <Pratyushshishodiya@gmail.com>
2022-07-28 21:06:29 +0530

    Setting up sql db

--- emp_app/models.py
+++ emp_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name=models.CharField(max_length=100,null=False)
+    location=models.CharField(max_length=100)
+
+class Role(models.Model):
+    name=models.CharField(max_length=100,null=False)
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100,null=False)
+    last_name = models.CharField(max_length=100)
+    dept= models.ForeignKey(Department,on_delete=models.CASCADE)
+    role=models.ForeignKey(Role,on_delete=models.CASCADE)
+    phone=models.CharField(max_length=100)
+    hire_date=models.CharField(max_length=100,null=False)
